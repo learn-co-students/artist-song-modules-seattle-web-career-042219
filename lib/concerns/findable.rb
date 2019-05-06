6d8e98206321c5edb43c3af0f3bff079d137cc53
Greg Morgan <gregory.morgan@flatironschool.com>
2019-05-05 17:54:39 -0700

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,7 @@
 module Findable
     module ClassMethods
-        def self.find_by_name
-            @@artists.detect{|a| a.name == name}
-            @@songs.detect{|a| a.name == name}
+        def find_by_name(name)
+            self.all.detect{|a| a.name == name}
         end
     end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -16,10 +16,6 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def self.all
     @@songs
   end
@@ -28,7 +24,4 @@ class Song
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
